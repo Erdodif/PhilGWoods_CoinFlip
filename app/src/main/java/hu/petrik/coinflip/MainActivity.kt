a3ba16ad7091ef51ea9f101f001d2d374cb0d915
Erdodif <erdodif@gmail.com>
2021-09-30 19:08:44 +0200

    4/a kivételével az összes elkészült

--- app/src/main/java/hu/petrik/coinflip/MainActivity.kt
+++ app/src/main/java/hu/petrik/coinflip/MainActivity.kt
@@ -1,11 +1,106 @@
 package hu.petrik.coinflip
 
+import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AlertDialog
+import kotlin.system.exitProcess
+import kotlin.random.Random
+import android.widget.Toast
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var dobasok: Int = 0
+    private var gyozelmek: Int = 0
+    private var veresegek: Int = 0
+    private var fej: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Locale.setDefault(Locale("hu"))
         setContentView(R.layout.activity_main)
+        init()
+        buttonFej.setOnClickListener {
+            tippel(true)
+        }
+        buttonIras.setOnClickListener {
+            tippel(false)
+        }
+    }
+
+    private fun szoveg(fej: Boolean): String {
+        var ki = "fej"
+        if (!fej) {
+            ki = "írás"
+        }
+        return ki
+    }
+
+    private fun init() {
+        fej = Random.nextBoolean()
+        dobasok = 0
+        gyozelmek = 0
+        veresegek = 0
+        lekovet()
+    }
+
+    private fun lekovet() {
+        textViewDobasok.text = ("Dobások: $dobasok")
+        textViewGyozelmek.text = ("Győzelem: $gyozelmek")
+        textViewVeresegek.text = ("Vereség: $veresegek")
+    }
+
+    private fun lekovet(tipp: Boolean) {
+        lekovet()
+        Toast.makeText(applicationContext, "Eredmény: " + szoveg(fej), Toast.LENGTH_SHORT).show()
+        if (dobasok > 3 && gyozelmek != veresegek) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Játék Vége")
+            var eredmeny = "Győzelem"
+            if (tipp == fej) {
+                eredmeny = "Vereség"
+            }
+            builder.setMessage("$eredmeny. Új játék?")
+            builder.setPositiveButton("Igen") { dialog, which ->
+                init()
+            }
+            builder.setNegativeButton("Nem") { dialog, which ->
+                exitProcess(0)
+            }
+            builder.show()
+        }
+    }
+
+
+    private fun megjelenit(fej: Boolean, animalt: Boolean) {
+        if (animalt) {
+            var jelenAllas = false
+            megjelenit(jelenAllas)
+            jelenAllas = !jelenAllas
+            megjelenit(jelenAllas)
+        }
+        megjelenit(fej)
+    }
+
+
+    private fun megjelenit(fej: Boolean) {
+        if (fej) {
+            imageViewErme.setImageResource(R.drawable.heads)
+        } else {
+            imageViewErme.setImageResource(R.drawable.tails)
+        }
+    }
+
+    private fun tippel(tipp: Boolean) {
+        if (tipp == fej) {
+            gyozelmek++
+        } else {
+            veresegek++
+        }
+        dobasok++
+        lekovet(tipp)
+        megjelenit(fej, true)
+        fej = Random.nextBoolean()
     }
 }
